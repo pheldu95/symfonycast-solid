0a08f584cf6c839567319d0405e18fd381832ba1
pheldu95 <phelps.duncan@gmail.com>
2023-04-05 12:25:37 -0500

    finished solid chapter 2. Added email confirmation but apparently the way we did it violates SRP because UserManager class now has too many responsibilities

--- src/Manager/UserManager.php
+++ src/Manager/UserManager.php
@@ -4,26 +4,58 @@ namespace App\Manager;
 
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserManager
 {
     private UserPasswordEncoderInterface $passwordEncoder;
     private EntityManagerInterface $entityManager;
+    private RouterInterface $router;
+    private MailerInterface $mailer;
 
-    public function __construct(UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager)
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder, EntityManagerInterface $entityManager, RouterInterface $router, MailerInterface $mailer)
     {
         $this->passwordEncoder = $passwordEncoder;
         $this->entityManager = $entityManager;
+        $this->router = $router;
+        $this->mailer = $mailer;
     }
 
     public function register(User $user, string $plainPassword): void
     {
+        $token = $this->createToken();
+        $user->setConfirmationToken($token);
+
+        $confirmationLink = $this->router->generate('check_confirmation_link',[
+            'token' => $user->getConfirmationToken(),
+        ],
+         UrlGeneratorInterface::ABSOLUTE_URL);
+
+        $confirmationEmail = (new TemplatedEmail())
+            ->from('staff@example.com')
+            ->to($user->getEmail())
+            ->subject('Confirm your account')
+            ->htmlTemplate('emails/registration_confirmation.html.twig')
+            ->context([
+                'confirmationLink' => $confirmationLink
+            ]);
+
         $user->setPassword(
             $this->passwordEncoder->encodePassword($user, $plainPassword)
         );
 
         $this->entityManager->persist($user);
         $this->entityManager->flush();
+
+        $this->mailer->send($confirmationEmail);
+    }
+
+    private function createToken(): string
+    {
+        return rtrim(strtr(base64_encode(random_bytes(32)), '+/', '-_'), '=');
     }
 }
